925de90d4a00fd30e5ab533340d917d68c951f80
NicolasAJ95 <nicolas.ajaramillo@live.com>
2018-07-26 21:49:08 -0500

    0.0.1
    Principios de camara y cambios de esta segun su estado, primer borrador del script principal del jugador, modelos de prueba

---
+++ Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float health;
+    private float specialMeter;
+
+    [SerializeField]
+    private bool drivingState;
+    [SerializeField]
+    private bool shootingState;
+
+    [SerializeField]
+    private Animator pAnimator;
+
+	// Use this for initialization
+	void Start () {
+        pAnimator = GetComponent<Animator>();
+        health = 100;
+        specialMeter = 10;
+        drivingState = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            pAnimator.SetBool("isShooting", true);
+            pAnimator.SetBool("isDriving", false);
+            shootingState = true;
+            drivingState = false;
+        } else if (Input.GetKeyUp(KeyCode .LeftShift))
+        {
+            pAnimator.SetBool("isShooting", false);
+            pAnimator.SetBool("isDriving", true);
+            shootingState = false;
+            drivingState = true;
+        }
+	}
+}
